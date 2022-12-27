1a76d1172597a59c4663ab71d759016e73cd59d1
Serhii Malychkovych <masevik@gmail.com>
2022-12-28 00:09:59 +0200

    refactoring by hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,62 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Box } from './Box';
 import { FeedbackOptions } from './FeedbackOptions';
 import { Notification } from './Notification';
 import { Section } from './Section';
 import { Statistics } from './Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleIncrement = options => {
+    switch (options) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        return;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        return;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        return;
+      default:
+        console.log('default');
+    }
   };
 
-  handleIncrement = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
-  };
-
-  // handleIncrement = event => {
-  //   const targetName = event.currentTarget.name;
-  //   this.setState(prevState => {
-  //     switch (targetName) {
-  //       case 'good':
-  //         return { good: prevState.good + 1 };
-  //       case 'neutral':
-  //         return { neutral: prevState.neutral + 1 };
-  //       case 'bad':
-  //         return { bad: prevState.bad + 1 };
-  //       default:
-  //         console.log('default');
-  //     }
-  //   });
-  // };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = countTotalFeedback => {
-    const { good, neutral } = this.state;
-    return this.countTotalFeedback() === 0
+  const countPositiveFeedbackPercentage = () => {
+    return countTotalFeedback() === 0
       ? 0
-      : Math.round(((good + neutral) * 100) / this.countTotalFeedback());
+      : Math.round(((good + neutral) * 100) / countTotalFeedback());
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <Box pl="100px" pt="20px">
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={{ good, neutral, bad }}
-            onLeaveFeedback={this.handleIncrement}
+  return (
+    <Box pl="100px" pt="20px">
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={{ good, neutral, bad }}
+          onLeaveFeedback={handleIncrement}
+        />
+      </Section>
+      {countTotalFeedback() === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
         </Section>
-        {this.countTotalFeedback === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        )}
-      </Box>
-    );
-  }
-}
+      )}
+    </Box>
+  );
+};
